f3db3aa0a7ad8770b3000eee4018934de14b1496
Simmi Jonsson <simmisj@hotmail.com>
2018-05-25 15:04:05 +0200

    Added an equals sign, doh.

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
     var date = new Date;
     console.log(date+"");
     var element = document.getElementById("answer");
-    if((date.getDay() == 5 && date.getHours() > 15) || date.getDay() == 6 || date.getDay() == 0){
+    if((date.getDay() == 5 && date.getHours() >= 15) || date.getDay() == 6 || date.getDay() == 0){
         element.innerHTML = "yes";
     }else{
         element.innerHTML = "no";
