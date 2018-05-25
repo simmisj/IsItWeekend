a7684c4391244fc5eebd0517a78b80c0a41917e6
Simmi Jonsson <simmisj@hotmail.com>
2018-05-25 12:46:31 +0200

    Logging the date to console for debuggin purposes.

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -2,8 +2,9 @@ $(document).ready(function () {
     var answer = document.getElementById("answer");
     answer.innerHTML = "haha2";
     var date = new Date;
+    console.log(date+"");
     var element = document.getElementById("answer");
-    if((date.getDay() == 5 && date.getHours() > 16) || date.getDay() == 6 || date.getDay() == 0){
+    if((date.getDay() == 5 && date.getHours() > 15) || date.getDay() == 6 || date.getDay() == 0){
         element.innerHTML = "yes";
     }else{
 element.innerHTML = "no";
